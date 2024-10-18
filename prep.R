6f3e5fd5852c10e1293c9d271c533daceb86d915
Brenden Smith <b@Brendens-MacBook-Air.local>
2024-10-18 15:25:19 -0400

    attempt creating json from secrets

--- prep.R
+++ prep.R
@@ -6,15 +6,38 @@ library(jsonlite)
 
 # Load the service account JSON from an environment variable
 
-service_account <- Sys.getenv("GOOGLE_SERVICE_ACCOUNT_JSON")
+library(jsonlite)
+
+type <- "service_account"
+project_id <- Sys.getenv("PROJECT_ID")
+private_key_id <- Sys.getenv("PRIVATE_KEY_ID")
+private_key <- Sys.getenv("PRIVATE_KEY")
+client_email <- Sys.getenv("CLIENT_EMAIL")
+client_id <- Sys.getenv("CLIENT_ID")
+auth_uri <- "https://accounts.google.com/o/oauth2/auth"
+token_uri <- "https://oauth2.googleapis.com/token"
+auth_provider_x509_cert_url <-  Sys.getenv("AUTH_PROVIDER_X509_CERT_URL")
+client_x509_cert_url <- Sys.getenv("CLIENT_X509_CERT_URL")
+universe_domain <-  "googleapis.com"
+
+service_account <- list(
+  type = type,
+  project_id = project_id,
+  private_key_id = private_key_id,
+  private_key = private_key,
+  client_email = client_email,
+  client_id = client_id,
+  auth_uri = auth_uri,
+  token_uri = token_uri,
+  auth_provider_x509_cert_url= auth_provider_x509_cert_url,
+  client_x509_cert_url = client_x509_cert_url,
+  universe_domain = universe_domain
+)
+
+json_content <- toJSON(service_account, pretty = TRUE, auto_unbox = TRUE)
 
 
-json_path <- tempfile(fileext = ".json")
-writeLines(service_account, json_path)
-
-
-# Authenticate using the service account key
-gs4_auth(path = json_path)
+gs4_auth(path = json_content)
 
 
 data <- read_sheet("https://docs.google.com/spreadsheets/d/1Qszy3C8C794LdQF3dAU4ZzENPqeYAGeHOQn8ajDt2Qs/edit?gid=635450401#gid=635450401", sheet = 2) |>
@@ -30,3 +53,9 @@ h <- sum |>
 
 monthly_breakdown <- long |>
   filter(month(due_date) == notif_num - 1)
+
+
+
+
+
+
