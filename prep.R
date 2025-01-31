7b098341ba0b04459f689ff20d6c88967e0fa1ff
Brenden Smith <b@Brendens-MacBook-Air.local>
2025-01-31 11:00:14 -0500

    update for 2025

--- prep.R
+++ prep.R
@@ -56,16 +56,16 @@ unlink(temp_file)
 # gs4_auth(path = json_content)
 
 
-data <- read_sheet("https://docs.google.com/spreadsheets/d/1Qszy3C8C794LdQF3dAU4ZzENPqeYAGeHOQn8ajDt2Qs/edit?gid=635450401#gid=635450401", sheet = 2) |>
+data <- read_sheet("https://docs.google.com/spreadsheets/d/1Qszy3C8C794LdQF3dAU4ZzENPqeYAGeHOQn8ajDt2Qs/edit?gid=635450401#gid=635450401", sheet = 3) |>
   janitor::clean_names()
 
 long <- data[,1:6]
-sum <- data[,13:16]
+sum <- data[,8:11]
 
 notif_num <- month(Sys.Date())
 
 h <- sum |>
-  filter(month(notify_16) == notif_num)
+  filter(month(notify) == notif_num)
 
 monthly_breakdown <- long |>
   filter(month(due_date) == notif_num - 1)
